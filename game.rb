77a5ba7941b0e8ce5a347d8db5ad77eadb3b4668
Dylan <dylandixon@hotmail.com>
2021-05-19 19:59:12 -0230

    Add logic for Maker class, Breaker class, and Board glass. Add logic to check a guess to see if the places are correct. Add logic to get guesses and code from Breaker and Maker classes.

--- game.rb
+++ game.rb
@@ -1,7 +1,66 @@
 class Board
+  def initialize(code)
+    @code = code
+  end
 
+  def print_game_board
+
+  end
+
+  def check_guess_for_value(guess)
+
+  end
+
+  def check_guess_for_place(guess)
+    index = 0
+    guess.reduce(0) do |sum, value|
+      if value == @code[index]
+        sum += 1
+      end
+      index += 1
+      sum
+    end
+  end
 end
 
-class Player
+class Maker
+  attr_reader :code, :board
 
+  def initialize(name)
+    @name = name
+    @code = []
+  end
+
+  def set_code
+    [nil, nil, nil, nil].each_with_index do |value, index|
+      puts "#{@name}, please enter your colour for number #{index + 1} in your code."
+      @code[index] = gets.chomp
+    end
+  end
+
+  def make_game_board
+    @board = Board.new(@code)
+  end
 end
+
+class Breaker
+  attr_accessor :name, :number_of_guesses
+
+  def initialize(name)
+    @name = name
+    @number_of_guesses = 0
+  end
+
+  def make_guesses
+    guess_array = []
+    [nil, nil, nil, nil].each_with_index do |value, index|
+      puts "#{@name}, please enter your guess for spot number #{index + 1}."
+      guess_array[index] = gets.chomp
+    end
+    guess_array
+  end
+end
+
+test = Board.new(['Y', 'R', 'G', 'B'])
+puts test.check_guess_for_value(['Y', 'R', 'G', 'B'])
+puts test.check_guess_for_place(['Y', 'R', 'G', 'B'])
